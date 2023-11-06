6fe319a4ebf449e9013ff5d230486b4f18f5fcf4
utsavsarkar <utsavsarkar2000@gmail.com>
2023-11-06 14:50:35 +0530

    updated change

--- social-media/src/routes/App.jsx
+++ social-media/src/routes/App.jsx
@@ -2,19 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../Components/Header";
 import SideBar from "../Components/SideBar";
 import "./App.css";
-import CreatePost from "../Components/CreatePost";
-import PostList from "../Components/PostList";
-import { useState } from "react";
 import PostListProvider from "../store/post-list-store";
 import { Outlet } from "react-router-dom";
 
 function App() {
-  const [selectedTab, setSelectedTab] = useState("Home");
-
   return (
     <PostListProvider>
       <div className="app-container">
-        <SideBar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+        <SideBar />
         <div className="content">
           <Header />
           <Outlet />
